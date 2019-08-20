2af2aa65761f0de3a77e66112e07601d2e11ad97
Renkinjutsu <lucas.cheng12@gmail.com>
2019-08-20 19:22:55 -0400

    webpack setup, build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
-};
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader:"html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+}
