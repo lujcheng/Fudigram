1eaec4bbc963872c86fd5c64a31e3359115e388c
Renkinjutsu <lucas.cheng12@gmail.com>
2019-08-21 23:54:23 -0400

    axios call function

--- src/app.js
+++ src/app.js
@@ -2,10 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import axios from 'axios'
 
+
 function App() {
+  axios.get('https://api.yelp.com/v3/businesses/search').then(res => {
+    console.log(res)
+  })
   return (
     <>
       <a href='/restaurants'> Search</a>
+
     </>
   )
 }
