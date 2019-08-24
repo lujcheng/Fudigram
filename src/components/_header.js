f697b5468c5717d03f1d2faa149be99945398f20
Renkinjutsu <lucas.cheng12@gmail.com>
2019-08-24 12:45:13 -0400

    initialy commit for header component

---
+++ src/components/_header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <>
+      <span>
+        FudiGram
+      </span>
+      <span>
+        <input type='text' placeholder='Search Friends' />
+      </span>
+      <span>
+        Explore
+      </span>
+      <span>
+        Login
+      </span>
+  )
+}
