b8a1dd2a47b44530303b9dcdf28b896b78df2c4b
Renkinjutsu <lucas.cheng12@gmail.com>
2019-08-24 12:49:08 -0400

    imported header into app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import axios from 'axios'
 import YelpSearch from './components/yelp-search'
+import Header from './components/_header'
 
 
 function App() {
