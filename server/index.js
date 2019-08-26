ee9d7eff43f2b44b1ef40d0b9c66cdfa6a0e7deb
Renkinjutsu <lucas.cheng12@gmail.com>
2019-08-26 17:41:41 -0400

    connecting db to users

--- server/index.js
+++ server/index.js
@@ -3,26 +3,34 @@ const app = express()
 
 const path = require('path')
 const React = require('react')
+require("dotenv").config({ path: __dirname + "/.env" });
 
 const PORT = 8080
 const DIST_DIR = path.join(__dirname, '../dist')
 const HTML_FILE = path.join(DIST_DIR, 'index.html')
 
-const models = require('../src/models')
-const connectDb = models.connectDb
+const {models, connectDb} = require('../src/models')
+// const connectDb = models.connectDb
 
-// connectDb().then(async () => {
-//   app.listen(process.env.PORT, () =>
-//     console.log(`Example app listening on port ${process.env.PORT}!`),
-//   );
-// });
+connectDb().then(async () => {
+  app.listen(process.env.PORT, () =>
+    console.log(`Example app listening on port ${process.env.PORT}!`),
+  );
+});
+
 
 app.use(express.static(DIST_DIR))
 
 app.get('/', function (req, res) {
-    res.sendFile(HTML_FILE)
+  res.sendFile(HTML_FILE)
+})
+
+app.post('/users', function (req, res) {
+  models.User.create(req.body).then((user) => {
+    res.send(user)
   })
 
+}) 
 const searchRouter = require('./routes/restaurants')
 app.use('/restaurants', searchRouter)
 
