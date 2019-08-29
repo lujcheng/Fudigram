28cbe86a7d5dd2c897f4e7b3b3a6137b96778699
Renkinjutsu <lucas.cheng12@gmail.com>
2019-08-29 19:59:10 -0400

    changed model creation

--- server/routes/registration.js
+++ server/routes/registration.js
@@ -1,15 +1,17 @@
 const router = require('express').Router()
-const User = require('../../src/models/user')
+const { models, connectDb } = require('../../src/models')
+const express = require('express')
+const app = express() 
 
 const path = require('path')
 const DIST_DIR = path.join(__dirname, '../../dist')
 const HTML_FILE = path.join(DIST_DIR, 'index.html')
 
-// connectDb().then(async () => {
-//   app.listen(process.env.PORT, () =>
-//     console.log(`Example app listening on port ${process.env.PORT}!`),
-//   );
-// });
+connectDb().then(async () => {
+  app.listen(process.env.PORT, () =>
+    console.log(`Example app listening on port ${process.env.PORT}!`),
+  );
+});
 
 router.route('/').get((req, res) => {
   res.sendFile(HTML_FILE)
@@ -17,10 +19,21 @@ router.route('/').get((req, res) => {
 
 router.route('/').post((req, res) => {
   console.log("new user: ", req.body)
-  res.send(req.body)
-  User.create(req.body).then((user) => {
-    res.send(user)
+  console.log("model: ", models)
+  res.send(req.body.userName)
+  let user = new models.User({username: req.body.userName})
+  user.save(err => {
+    if (err) { 
+      throw err
+    } else {
+      res.send('saved')
+    }
   })
+ 
+  // .then((err, user) => {
+  //   if (err) return handleError(err)
+  //   res.send(user)
+  // })
 })
 
 module.exports = router;
