066bbee25c0e9eeaf1361fc544299a3a7186df80
Renkinjutsu <lucas.cheng12@gmail.com>
2019-08-28 20:38:12 -0400

    changed index dir, webpack entry

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const htmlPlugin = new HtmlWebPackPlugin({
 });
 
 module.exports = {
-  entry: "./src/app.js",
+  entry: "./src/ReactApp/app.js",
   output: { // NEW
     path: path.join(__dirname, 'dist'),
     filename: "[name].js"
