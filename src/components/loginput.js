60c3083971bebbcb5ecea65d7779f7521d60fc4c
Renkinjutsu <lucas.cheng12@gmail.com>
2019-09-23 22:11:22 -0400

    created login input component

---
+++ src/components/loginput.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+export default function UserInput() {
+  const [term, setTerm] = useState('')
+  const [results, setResults] = useState(0)
+
+  const handleOnChange = e => {
+    setTerm(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    setResults(results + 1)
+
+  }
+
+  return (
+    <>
+      <p>Search Term: {term}</p>
+      <form action='/users' method='post'>
+        User Name:
+        <input type='text' name='userName' onChange={handleOnChange} />
+        <input type='submit' value='Submit'/>
+      </form>
+      <p>Search results: {results} </p>
+
+    </>
+  )
+}
