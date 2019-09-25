6bb7769d2fbe87cac120db2efa677f009b1b36bd
Renkinjutsu <lucas.cheng12@gmail.com>
2019-09-24 22:39:36 -0400

    reuse userinput for both registration and login

--- src/ReactApp/pages/users.js
+++ src/ReactApp/pages/users.js
@@ -3,10 +3,11 @@ import { Route, Switch } from 'react-router-dom'
 import UserInput from '../../components/user-input'
 
 function Users() {
+  let input = UserInput('/users')
   return (
     <>
       <p>this is user page</p>
-      <UserInput />
+      {input}
     </>
   )
 }
