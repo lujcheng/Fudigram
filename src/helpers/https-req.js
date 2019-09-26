8b0c36ad3788c7aa37188d0cad7af513dc4b0ad9
Renkinjutsu <lucas.cheng12@gmail.com>
2019-09-26 19:33:32 -0400

    linting

--- src/helpers/https-req.js
+++
@@ -1,5 +0,0 @@
-const https = require('https')
-
-module.exports = function httpsReq(link) {
-  return link
-}
--- src/helpers/yelp-display.js
+++ src/helpers/yelp-display.js
@@ -22,6 +22,5 @@ export default function yelpDis(dataArr) {
       )
     })
     return display
-
   }
 }
