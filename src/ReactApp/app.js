9fb2f4dba533868df518818be8a770830cba81a0
Renkinjutsu <lucas.cheng12@gmail.com>
2019-09-28 23:45:09 -0400

    removed imports from app.js

--- src/ReactApp/app.js
+++ src/ReactApp/app.js
@@ -3,10 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 import { Home, Users } from './pages'
 import Registration from './pages/registration'
 import _header from '../components/_header'
-// import ReactDOM from 'react-dom'
-// import axios from 'axios'
-// import YelpSearch from './components/yelp-search'
-// import Header from './components/_header'
+
 
 
 const App = () => (
